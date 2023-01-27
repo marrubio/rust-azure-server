2f44d7680aa4ff90f1f2b2d1807aba3e7e1ab5e5
Mario Rubio <marugi@gmail.com>
2023-01-27 21:01:38 +0100

    Initial commit

---
+++ powershell-script.ps1
@@ -0,0 +1,9 @@
+$rgName ='rust-server-rg'
+$location = 'francecentral'
+New-AzResourceGroup -Name $rgName -Location $location
+
+New-AzResourceGroupDeployment `
+-ResourceGroupName $rgName `
+-TemplateUri "https://raw.githubusercontent.com/marrubio/rust-azure-server/main/rust-server-template.json" `
+-TemplateParameterUri "https://raw.githubusercontent.com//marrubio/rust-azure-server/blob/main/rust-server-parameters.json" `
+-AsJob
